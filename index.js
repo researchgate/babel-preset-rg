e1fa252c2622b12a2ea896132737761e25376253
Luis Merino <mail@luismerino.name>
2017-08-10 17:30:50 +0200

    style(*): Prettify code

--- index.js
+++ index.js
@@ -1,71 +1,68 @@
-"use strict";
+'use strict';
 
 module.exports = function(context, options) {
   if (!options) options = {};
 
   const env = process.env.BABEL_ENV || process.env.NODE_ENV;
-  if (env !== "development" && env !== "test" && env !== "production") {
+  if (env !== 'development' && env !== 'test' && env !== 'production') {
     throw new Error(
-      "Using `@researchgate/babel-preset-rg` requires that you specify `NODE_ENV` or " +
+      'Using `@researchgate/babel-preset-rg` requires that you specify `NODE_ENV` or ' +
         '`BABEL_ENV` environment variables. Valid values are "development", ' +
         '"test", and "production". Instead, received: ' +
         JSON.stringify(env) +
-        ".",
+        '.'
     );
   }
 
-  const type = process.env.BABEL_OUTPUT || (env === "test" ? "cjs" : "esm");
-  if (type !== "esm" && type !== "cjs") {
+  const type = process.env.BABEL_OUTPUT || (env === 'test' ? 'cjs' : 'esm');
+  if (type !== 'esm' && type !== 'cjs') {
     throw new Error(
-      "Invalid value for the `BABEL_OUTPUT` environment variable." +
+      'Invalid value for the `BABEL_OUTPUT` environment variable.' +
         ' Valid values are "esm", and "cjs". Instead, received: ' +
         JSON.stringify(type) +
-        ".",
+        '.'
     );
   }
 
   const envOptions = Object.assign(
     {
       loose: true,
-      modules: type === "cjs" ? "commonjs" : false,
+      modules: type === 'cjs' ? 'commonjs' : false,
     },
-    options.env || {},
+    options.env || {}
   );
 
-  const presets = [
-    [require.resolve("babel-preset-env"), envOptions],
-    require.resolve("babel-preset-react"),
-  ];
+  const presets = [[require.resolve('babel-preset-env'), envOptions], require.resolve('babel-preset-react')];
 
   const plugins = [
-    require.resolve("babel-plugin-transform-class-properties"),
+    require.resolve('babel-plugin-transform-class-properties'),
     [
-      require.resolve("babel-plugin-transform-object-rest-spread"),
+      require.resolve('babel-plugin-transform-object-rest-spread'),
       {
         useBuiltIns: true,
       },
     ],
-    require.resolve("babel-plugin-lodash"),
+    require.resolve('babel-plugin-lodash'),
     [
-      require.resolve("babel-plugin-transform-react-remove-prop-types"),
+      require.resolve('babel-plugin-transform-react-remove-prop-types'),
       {
-        mode: "unsafe-wrap",
+        mode: 'unsafe-wrap',
       },
     ],
     [
-      require.resolve("babel-plugin-transform-react-jsx"),
+      require.resolve('babel-plugin-transform-react-jsx'),
       {
         useBuiltIns: true,
       },
     ],
   ];
 
-  if (env === "development") {
+  if (env === 'development') {
     plugins.push.apply(plugins, [
-      require.resolve("babel-plugin-transform-react-jsx-source"),
-      require.resolve("babel-plugin-transform-react-jsx-self"),
+      require.resolve('babel-plugin-transform-react-jsx-source'),
+      require.resolve('babel-plugin-transform-react-jsx-self'),
     ]);
-    presets.push(require.resolve("babel-preset-react-hmre"));
+    presets.push(require.resolve('babel-preset-react-hmre'));
   }
 
   return {
