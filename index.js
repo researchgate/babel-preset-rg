d7f741bc05ab11cb3b5e84ab0e4d870db851dd44
Daniel Tschinder <code@tschinder.de>
2017-10-10 22:54:56 +0200

    chore(style): Add editorconfig and use 4 spaces indentation

--- index.js
+++ index.js
@@ -1,72 +1,72 @@
 'use strict';
 
 module.exports = function(context, options) {
-  if (!options) options = {};
+    if (!options) options = {};
 
-  const env = process.env.BABEL_ENV || process.env.NODE_ENV;
-  if (env !== 'development' && env !== 'test' && env !== 'production') {
-    throw new Error(
-      'Using `@researchgate/babel-preset-rg` requires that you specify `NODE_ENV` or ' +
-        '`BABEL_ENV` environment variables. Valid values are "development", ' +
-        '"test", and "production". Instead, received: ' +
-        JSON.stringify(env) +
-        '.'
+    const env = process.env.BABEL_ENV || process.env.NODE_ENV;
+    if (env !== 'development' && env !== 'test' && env !== 'production') {
+        throw new Error(
+            'Using `@researchgate/babel-preset-rg` requires that you specify `NODE_ENV` or ' +
+                '`BABEL_ENV` environment variables. Valid values are "development", ' +
+                '"test", and "production". Instead, received: ' +
+                JSON.stringify(env) +
+                '.'
+        );
+    }
+
+    const type = process.env.BABEL_OUTPUT || (env === 'test' ? 'cjs' : 'esm');
+    if (type !== 'esm' && type !== 'cjs') {
+        throw new Error(
+            'Invalid value for the `BABEL_OUTPUT` environment variable.' +
+                ' Valid values are "esm", and "cjs". Instead, received: ' +
+                JSON.stringify(type) +
+                '.'
+        );
+    }
+
+    const envOptions = Object.assign(
+        {
+            loose: true,
+            modules: type === 'cjs' ? 'commonjs' : false,
+        },
+        options.env || {}
     );
-  }
 
-  const type = process.env.BABEL_OUTPUT || (env === 'test' ? 'cjs' : 'esm');
-  if (type !== 'esm' && type !== 'cjs') {
-    throw new Error(
-      'Invalid value for the `BABEL_OUTPUT` environment variable.' +
-        ' Valid values are "esm", and "cjs". Instead, received: ' +
-        JSON.stringify(type) +
-        '.'
-    );
-  }
+    const presets = [[require.resolve('babel-preset-env'), envOptions], require.resolve('babel-preset-react')];
 
-  const envOptions = Object.assign(
-    {
-      loose: true,
-      modules: type === 'cjs' ? 'commonjs' : false,
-    },
-    options.env || {}
-  );
+    const plugins = [
+        require.resolve('babel-plugin-transform-class-properties'),
+        [
+            require.resolve('babel-plugin-transform-object-rest-spread'),
+            {
+                useBuiltIns: true,
+            },
+        ],
+        require.resolve('babel-plugin-lodash'),
+        [
+            require.resolve('babel-plugin-transform-react-remove-prop-types'),
+            {
+                mode: 'unsafe-wrap',
+            },
+        ],
+        [
+            require.resolve('babel-plugin-transform-react-jsx'),
+            {
+                useBuiltIns: true,
+            },
+        ],
+    ];
 
-  const presets = [[require.resolve('babel-preset-env'), envOptions], require.resolve('babel-preset-react')];
+    if (env === 'development') {
+        plugins.push.apply(plugins, [
+            require.resolve('babel-plugin-transform-react-jsx-source'),
+            require.resolve('babel-plugin-transform-react-jsx-self'),
+        ]);
+        presets.push(require.resolve('babel-preset-react-hmre'));
+    }
 
-  const plugins = [
-    require.resolve('babel-plugin-transform-class-properties'),
-    [
-      require.resolve('babel-plugin-transform-object-rest-spread'),
-      {
-        useBuiltIns: true,
-      },
-    ],
-    require.resolve('babel-plugin-lodash'),
-    [
-      require.resolve('babel-plugin-transform-react-remove-prop-types'),
-      {
-        mode: 'unsafe-wrap',
-      },
-    ],
-    [
-      require.resolve('babel-plugin-transform-react-jsx'),
-      {
-        useBuiltIns: true,
-      },
-    ],
-  ];
-
-  if (env === 'development') {
-    plugins.push.apply(plugins, [
-      require.resolve('babel-plugin-transform-react-jsx-source'),
-      require.resolve('babel-plugin-transform-react-jsx-self'),
-    ]);
-    presets.push(require.resolve('babel-preset-react-hmre'));
-  }
-
-  return {
-    presets: presets,
-    plugins: plugins,
-  };
+    return {
+        presets: presets,
+        plugins: plugins,
+    };
 };
